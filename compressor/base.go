8c81e3043c857a9bf739aef5d646a92abee20ec9
zeroleo12345 <zeroleo12345@163.com>
2025-11-01 13:54:50 +0800

    remove print

--- compressor/base.go
+++ compressor/base.go
@@ -28,7 +28,6 @@ type Compressor interface {
 
 func (c *Base) archiveFilePath(ext string) string {
     format := c.model.CompressWith.Viper.GetString("format")
-    fmt.Println("format: ", format)
     return filepath.Join(c.model.TempPath, time.Now().Format(format)+ext)
 }
 
